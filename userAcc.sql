325a428f43e94b5caa91ec9a6c9ef96442a5e61b
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-12-11 15:12:40 -0500

    Made come corrections

--- userAcc.sql
+++ userAcc.sql
@@ -1,1 +1,2 @@
-GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON . TO 'regular_user'@'localhost';
+CREATE USER regular_user;
+GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'regular_user'@'localhost';
