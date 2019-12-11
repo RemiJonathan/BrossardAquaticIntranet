d403bcb6d5afe27d772f293d1392c361216ed421
Horia Cristian Sandu <horiasandu1@gmail.com>
2019-12-11 13:41:33 -0500

    Updated connection string

--- db_operations/connection.php
+++ db_operations/connection.php
@@ -2,7 +2,7 @@
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'regular_user');
-define('DB_PASSWORD', 'codeBulle123');
+define('DB_PASSWORD', '2TUTImmAun7jfXQt');
 define('DB_DATABASE', 'brossard_intranet');
 
 $connection = @new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_DATABASE);
