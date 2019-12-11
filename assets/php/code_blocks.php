d9c492cf0425f43a2619eedc06d27f70a89a5a95
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-12-11 11:05:22 -0500

    Updated Template and configured files

--- assets/php/code_blocks.php
+++ assets/php/code_blocks.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @param $title will set the text at the top of the page
+ * @param $title String will set the text at the top of the page
  */
 function block_print_document_header($title)
 {
@@ -15,10 +15,32 @@ function block_print_document_header($title)
     ";
 }
 
+function block_print_header(){
+    echo "
+    <!-- Header -->
+	<header class=\"alt\" id=\"header\">
+		<span class=\"logo\"><a href=\"index.html\">Brossard <span>Secteur Aquatic</span></a></span>
+		<a href=\"#menu\"><span>Menu</span></a>
+	</header>
+    ";
+}
+
 
 function block_print_footer()
 {
     echo "";
+}
+
+function block_print_copyright(){
+    echo "
+    <!-- Copyright -->
+	<section class=\"wrapper style2 copyright\">
+		<div class=\"inner\">
+			Oeuvre In&eacute;dite &copy; <span id='current_year'></span> Intranet Du Secteur Aquatique. Projet De Fin D’Études Collégiales De <a href='https://remijonathan.com' target='_blank'>Rémi Jonathan Choquette</a> & <a href='https://www.linkedin.com/in/horia-sandu-7a7631162/' target='_blank'>Horia Cristian Sandu</a>.
+		</div>
+		<script>document.getElementById(\"current_year\").innerHTML = new Date().getFullYear();</script>
+	</section>
+    ";
 }
 
 function block_print_scripts(){
--- template.php
+++ template.php
@@ -3,6 +3,7 @@ include ("assets/php/code_blocks.php");
 
 
 echo "<!DOCTYPE HTML><html>";
+block_print_document_header("Template");
 echo "<body class=\"is-preload\">";
 echo "<!-- Page wrapper -->
           <div id=\"page-wrapper\">";
@@ -12,5 +13,6 @@ echo "<!-- Page wrapper -->
  */
 
 echo "    </div>";
+block_print_scripts();
 echo "	</body>";
 echo "</html>";
