e5aa9a405b810484734cfb50223baecc17c72106
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-12-11 16:07:52 -0500

    Created News Feed

--- file_operations/document_update.php
+++ file_operations/document_update.php
@@ -23,7 +23,7 @@ block_print_header("");
 //TODO: put logic to verify cookie and modify this
 block_print_nav("<li><a href=\"login.php\">Connexion</a></li>");
 //put code in content
-block_print_main("");
+block_print_main($form_data);
 
 block_print_copyright();
 echo "    </div>";
