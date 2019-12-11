1f9815794d2cc6d0e019b98917de8cd200d60c6a
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-12-11 13:46:40 -0500

    Changed DB mnemonics

--- db_operations/connection.php
+++ db_operations/connection.php
@@ -5,9 +5,9 @@ define('DB_USERNAME', 'regular_user');
 define('DB_PASSWORD', '2TUTImmAun7jfXQt');
 define('DB_DATABASE', 'brossard_intranet');
 
-$connection = @new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_DATABASE);
+$db = @new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_DATABASE);
 
-if($connection->connect_error){
-echo "Connection failed: $connection->connect_error";
+if($db->connect_error){
+echo "Connection failed: $db->connect_error";
 exit;
 }
