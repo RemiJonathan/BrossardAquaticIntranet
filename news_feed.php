534c3f7e3164a91a77aeafa648cdd9c1ffb66a5a
RemiJonathan <contact@remijonathan.com>
2019-12-11 23:05:16 -0500

    Handle no session connections

--- news_feed.php
+++ news_feed.php
@@ -1,53 +1,22 @@
 <?php
-include ("assets/php/code_blocks.php");
-include ("db_operations/connection.php");
+define('PREAMBLE', '');
+include (PREAMBLE."assets/php/code_blocks.php");
+include (PREAMBLE."db_operations/connection.php");
 
 echo "<!DOCTYPE HTML><html>";
 block_print_document_header("Acceuil",'');
 echo "<body class=\"is-preload\">";
 echo "<div id=\"page-wrapper\">";
 block_print_header("");
+session_start();
+if(isset($_SESSION['user_id'])) {
 
-block_print_main("
-    <form class=\"form-horizontal\" action=\"\" method=\"post\" name=\"uploadCSV\"
-    enctype=\"multipart/form-data\">
-    <div class=\"input-row\">
-        <label class=\"col-md-4 control-label\">Choose CSV File</label> <input
-            type=\"file\" name=\"file\" id=\"file\" accept=\".csv\">
-        <button type=\"submit\" id=\"submit\" name=\"import\"
-            class=\"btn-submit\">Import</button>
-        <br />
+}else{
+    block_print_nav("<li><a href='login.php'>Connexion</a></li>");
 
-    </div>
-    <div id=\"labelError\"></div>
-</form>
-
-<script type=\"text/javascript\">
-	$(document).ready(
-	function() {
-		$(\"#frmCSVImport\").on(
-		\"submit\",
-		function() {
-
-			$(\"#response\").attr(\"class\", \"\");
-			$(\"#response\").html(\"\");
-			var fileType = \".csv\";
-			var regex = new RegExp(\"([a-zA-Z0-9\s_\\.\-:])+(\"
-					+ fileType + \")$\");
-			if (!regex.test($(\"#file\").val().toLowerCase())) {
-				$(\"#response\").addClass(\"error\");
-				$(\"#response\").addClass(\"display-block\");
-				$(\"#response\").html(
-						\"Invalid File. Upload : <b>\" + fileType
-								+ \"</b> Files.\");
-				return false;
-			}
-			return true;
-		});
-	});
-</script>
-");
-
+    block_print_main("<h2>D&eacute;sol&eacute;, la session est expir&eacute;e ou inexistante</h2>".$connection_form);
+    session_destroy();
+}
 block_print_copyright();
 echo "    </div>";
 block_print_scripts('');
--- validate_registration.php
+++ validate_registration.php
@@ -1,9 +1,10 @@
 <?php
 include ("assets/php/code_blocks.php");
 include ("db_operations/connection.php");
-session_start();
+session_start(['cookie_lifetime' => 6000]);
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
+
     // username and password sent from form
 
     $myusername = mysqli_real_escape_string($db,$_POST['username']);
@@ -14,20 +15,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $passwordhash = password_hash($mypassword, PASSWORD_BCRYPT, $options);
     echo $passwordhash;
 
-    $sql = "SELECT id FROM admin WHERE username = '$myusername' and passhash = '$passwordhash'";
+    $sql = "SELECT id FROM admin WHERE username = '$myusername'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     $active = $row['active'];
-
     $count = mysqli_num_rows($result);
 
     // If result matched $myusername and $mypassword, table row must be 1 row
 
-    if($count == 1) {
-        session_register("myusername");
+    if($count == 0) {
         $_SESSION['login_user'] = $myusername;
 
-        header("location: welcome.php");
+        header("Location: news_feed.php");
     }else {
         $error = "Your Login Name or Password is invalid";
     }
