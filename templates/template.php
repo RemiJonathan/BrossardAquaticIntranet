a0ed0b974c07a46ffce450904d9c6dcb25d0b9ef
RemiJonathan <contact@remijonathan.com>
2019-12-11 23:05:30 -0500

    Cleaned Templates

--- templates/template.php
+++ templates/template.php
@@ -1,18 +1,20 @@
 <?php
+define('PREAMBLE', '../');
 include ("../assets/php/code_blocks.php");
 
 echo "<!DOCTYPE HTML><html>";
-block_print_document_header("Template");
+block_print_document_header("Template", PREAMBLE);
 echo "<body class=\"is-preload\">";
 echo "<div id=\"page-wrapper\">";
 block_print_header("");
 //TODO: put logic to verify cookie and modify this
-block_print_nav("<li><a href=\"login.php\">Connexion</a></li>");
+block_print_nav("<li><a href=\"".PREAMBLE."login.php\">Connexion</a></li>");
+
 //put code in content
 block_print_main("");
 
 block_print_copyright();
 echo "    </div>";
-block_print_scripts();
+block_print_scripts(PREAMBLE);
 echo "	</body>";
 echo "</html>";
