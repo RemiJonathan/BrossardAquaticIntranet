784191faef00657863b87372718228e8caaf39cb
RemiJonathan <contact@remijonathan.com>
2019-12-12 20:15:05 -0500

    Handle invalid password

--- validate_connection.php
+++ validate_connection.php
@@ -23,8 +23,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['user_id'] = $myusername;
             header('Location: news_feed.php');
         }else{
-            $error = "Invalid Password";
+            $error = "Mot de passe invalide";
             //Todo Implement invalid credentials
+            header("Location: login.php?error=$error");
         }
     }else{
         $error = utf8_encode("NIE invalide");
