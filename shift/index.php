43d5dbb32e336cd8256e36a0414bb98cfb862630
Rémi Jonathan Choquette <contact@remijonathan.com>
2020-01-14 16:19:52 -0500

    Fixed the table

--- shift/index.php
+++ shift/index.php
@@ -25,19 +25,11 @@ if (isset($_SESSION['user_id'])) {
 
         $content .= "<table style='empty-cells: show' class=\"alt\">
     <tbody>
-        <tr>
-            <th style='width: 8%'></th>
-            <th id='weekDay0' style='width: 12.85%'>Dimanche</th>
-            <th id='weekDay1' style='width: 12.85%'>Lundi</th>
-            <th id='weekDay2' style='width: 12.85%'>Mardi</th>
-            <th id='weekDay3' style='width: 12.85%'>Mercredi</th>
-            <th id='weekDay4' style='width: 12.85%'>Jeudi</th>
-            <th id='weekDay5' style='width: 12.85%'>Vendredi</th>
-            <th id='weekDay6' style='width: 12.85%'>Samedi</th>
-        </tr>";
+    <tr><th>Dimanche</th></tr>
+        ";
 
         $convertedWeekString = $_GET['week'][0] . $_GET['week'][1] . $_GET['week'][2] . $_GET['week'][3] . $_GET['week'][5] . $_GET['week'][6] . $_GET['week'][7];
-        $content .= '<tr><th></th>';
+        $content .= '<tr><th></th></tr>';
         for ($i = 0; $i < 7; $i++) {
             switch ($i) {
                 case 0:
@@ -47,14 +39,14 @@ if (isset($_SESSION['user_id'])) {
                     $lastday = date('y-m-d', strtotime($convertedWeekString . $i));
                     break;
             }
-            $content .= "<th><small class='small'>" . date('d/m/y', strtotime($convertedWeekString . $i)) . "</small></th>";
+            //$content .= "<th><small class='small'>" . date('d/m/y', strtotime($convertedWeekString . $i)) . "</small></th>";
         }
-        $content .= '</tr>';
+        //$content .= '</tr>';
 
-        $this_weeks_shifts_sql = "SELECT * FROM shift WHERE start_date <= '$firstday' AND end_date >= '$lastday'";
+        $this_weeks_shifts_sql = "SELECT * FROM shift WHERE start_date <= '$firstday' AND end_date >= '$lastday' AND day = 'Dimanche'";
         $this_weeks_earliest_shift_sql = $this_weeks_shifts_sql . " ORDER BY start_time";
         $this_weeks_latest_ending_shift_sql = $this_weeks_shifts_sql . " ORDER BY end_time DESC";
-        $this_weeks_shifts_res = $db->query($this_weeks_shifts_sql);
+        $this_weeks_shifts_res = $db->query($this_weeks_shifts_sql." ORDER BY start_time");
 
         $table_begin = $db->query($this_weeks_earliest_shift_sql)->fetch_array()['start_time'];
         $table_end = $db->query($this_weeks_latest_ending_shift_sql)->fetch_array()['end_time'];
@@ -69,7 +61,7 @@ if (isset($_SESSION['user_id'])) {
             $shift_array[$index] = $shift;
 
             $duration = round(abs($end_time - $start_time) / 60, 2);
-            $shift_array[$index]['html'] = '<td class="shift" id="' . $shift['shift_id'] . '" rowspan="' . (($duration / 15)) . '">' . $shift['description'] . '</td>';
+            $shift_array[$index]['html'] = '<td class="shift" id="' . $shift['shift_id'] . '" rowspan="' . (($duration / 5)) . '">' . $shift['description'] . '</td>';
 
             $shift_array[$index]['duration'] = $duration;
 
@@ -78,21 +70,21 @@ if (isset($_SESSION['user_id'])) {
 
         //print_r($shift_array);
 
-        for ($i = strtotime($table_begin); $i <= strtotime($table_end); $i += (15 * 60)) {
+        for ($i = strtotime($table_begin); $i <= strtotime($table_end); $i += (5 * 60)) {
             $content .= "<tr>";
-            for ($j = 0; $j < 8; $j++) {
+            for ($j = 0; $j < 2; $j++) {
                 switch ($j) {
                     case 0:
                         //Show the hour
-                        if ($i % 1800 == 0) {
-                            $content .= "<th rowspan='2'>" . date('G:i', $i) . "</th>";
+                        if ($i % 300 == 0) {
+                            $content .= "<th rowspan=''>" . date('G:i', $i) . "</th>";
                         } else {
                             //$content .= "<th><i style='color:#fff;'>15</i></th>";
                         }
                         break;
-                    case 1:
+                    default:
                         $number = 0;
-                        foreach ($shift_array as $shift) {
+                        foreach ($shift_array as $key => $shift) {
 
                             if (strcmp($shift['day'], 'Dimanche') == 0) {
                                 if (date($shift['start_time']) == date('H:i:s', $i)) {
@@ -100,113 +92,114 @@ if (isset($_SESSION['user_id'])) {
                                     $number++;
                                 } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
                                     $number++;
+                                } else {
+                                    //If the current shift has ended but the next shift has not started
+
+                                    //$content .= "<td></td>";
                                 }
                             }
 
                         }
-                        if ($number == 0) $content .= "<td></td>";
+                        //if ($number == 0) $content .= "<td></td>";
                         break;
-                    case 2:
-                        $number = 0;
-                        foreach ($shift_array as $shift) {
+                    /*
+                case 2:
+                    $number = 0;
+                    foreach ($shift_array as $shift) {
 
-                            if (strcmp($shift['day'], 'Lundi') == 0) {
-                                if (date($shift['start_time']) == date('H:i:s', $i)) {
-                                    $content .= $shift['html'];
-                                    $number++;
-                                } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
-                                    $number++;
-                                }
+                        if (strcmp($shift['day'], 'Lundi') == 0) {
+                            if (date($shift['start_time']) == date('H:i:s', $i)) {
+                                $content .= $shift['html'];
+                                $number++;
+                            } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
+                                $number++;
                             }
-
                         }
-                        if ($number == 0) $content .= "<td></td>";
-                        break;
-                    case 3:
-                        $number = 0;
-                        foreach ($shift_array as $shift) {
 
-                            if (strcmp($shift['day'], 'Mardi') == 0) {
-                                if (date($shift['start_time']) == date('H:i:s', $i)) {
-                                    $content .= $shift['html'];
-                                    $number++;
-                                } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
-                                    $number++;
-                                }
+                    }
+                    if ($number == 0) $content .= "<td></td>";
+                    break;
+                case 3:
+                    $number = 0;
+                    foreach ($shift_array as $shift) {
+
+                        if (strcmp($shift['day'], 'Mardi') == 0) {
+                            if (date($shift['start_time']) == date('H:i:s', $i)) {
+                                $content .= $shift['html'];
+                                $number++;
+                            } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
+                                $number++;
                             }
-
                         }
-                        if ($number == 0) $content .= "<td></td>";
-                        break;
-                    case 4:
-                        $number = 0;
-                        foreach ($shift_array as $shift) {
 
-                            if (strcmp($shift['day'], 'Mercredi') == 0) {
-                                if (date($shift['start_time']) == date('H:i:s', $i)) {
-                                    $content .= $shift['html'];
-                                    $number++;
-                                } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
-                                    $number++;
-                                }
+                    }
+                    if ($number == 0) $content .= "<td></td>";
+                    break;
+                case 4:
+                    $number = 0;
+                    foreach ($shift_array as $shift) {
+
+                        if (strcmp($shift['day'], 'Mercredi') == 0) {
+                            if (date($shift['start_time']) == date('H:i:s', $i)) {
+                                $content .= $shift['html'];
+                                $number++;
+                            } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
+                                $number++;
                             }
-
                         }
-                        if ($number == 0) $content .= "<td></td>";
-                        break;
-                    case 5:
-                        $number = 0;
-                        foreach ($shift_array as $shift) {
 
-                            if (strcmp($shift['day'], 'Jeudi') == 0) {
-                                if (date($shift['start_time']) == date('H:i:s', $i)) {
-                                    $content .= $shift['html'];
-                                    $number++;
-                                } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
-                                    $number++;
-                                }
+                    }
+                    if ($number == 0) $content .= "<td></td>";
+                    break;
+                case 5:
+                    $number = 0;
+                    foreach ($shift_array as $shift) {
+
+                        if (strcmp($shift['day'], 'Jeudi') == 0) {
+                            if (date($shift['start_time']) == date('H:i:s', $i)) {
+                                $content .= $shift['html'];
+                                $number++;
+                            } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
+                                $number++;
                             }
-
                         }
-                        if ($number == 0) $content .= "<td></td>";
-                        break;
 
-                    case 6:
-                        $number = 0;
-                        foreach ($shift_array as $shift) {
+                    }
+                    if ($number == 0) $content .= "<td></td>";
+                    break;
 
-                            if (strcmp($shift['day'], 'Vendredi') == 0) {
-                                if (date($shift['start_time']) == date('H:i:s', $i)) {
-                                    $content .= $shift['html'];
-                                    $number++;
-                                } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
-                                    $number++;
-                                }
+                case 6:
+                    $number = 0;
+                    foreach ($shift_array as $shift) {
+
+                        if (strcmp($shift['day'], 'Vendredi') == 0) {
+                            if (date($shift['start_time']) == date('H:i:s', $i)) {
+                                $content .= $shift['html'];
+                                $number++;
+                            } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
+                                $number++;
                             }
-
                         }
-                        if ($number == 0) $content .= "<td></td>";
-                        break;
-                    case 7:
-                        $number = 0;
-                        foreach ($shift_array as $shift) {
 
-                            if (strcmp($shift['day'], 'Samedi') == 0) {
-                                if (date($shift['start_time']) == date('H:i:s', $i)) {
-                                    $content .= $shift['html'];
-                                    $number++;
-                                } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
-                                    $number++;
-                                }
+                    }
+                    if ($number == 0) $content .= "<td></td>";
+                    break;
+                case 7:
+                    $number = 0;
+                    foreach ($shift_array as $shift) {
+
+                        if (strcmp($shift['day'], 'Samedi') == 0) {
+                            if (date($shift['start_time']) == date('H:i:s', $i)) {
+                                $content .= $shift['html'];
+                                $number++;
+                            } else if (date($shift['end_time']) > date('H:i:s', $i) && date($shift['start_time']) < date('H:i:s', $i)) {
+                                $number++;
                             }
-
                         }
-                        if ($number == 0) $content .= "<td></td>";
-                        break;
 
-                    default:
-                        $content .= "<td></td>";
-                        break;
+                    }
+                    if ($number == 0) $content .= "<td></td>";
+                    break;*/
 
                 }
 
