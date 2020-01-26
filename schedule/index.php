827b01ae27d39a25261e08bd2269519b382a7014
RemiJonathan <contact@remijonathan.com>
2020-01-25 23:31:15 -0500

    Implemented Session selection on schedule modification

--- schedule/index.php
+++ schedule/index.php
@@ -21,36 +21,62 @@ if (isset($_SESSION['user_id'])) {
 						<div class=\"inner\">";
 
         echo "<h1>Gestion d'horaire</h1>";
+        $result = $db->query("SELECT * FROM schedule;");
+        $form_data_shift_list = "Session: <form method='post'><select onchange='this.form.submit()' name='schedule'>";
 
-        $locationArray = getLocations($db);
-        echo "<form><div class=\"row gtr-uniform\"><div class=\"col-12\"><ul class=\"nav nav-tabs\">";
-        $counter = 0;
-        foreach ($locationArray as $location) {
-            if (isset($_GET['location'])) {
-                if ($_GET['location'] == $location) {
-                    echo "<li class=\"nav-item\">
+        $form_data_shift_list .= '<option selected disabled> -- Choisissez une session -- </option>';
+        if (isset($_POST['schedule'])) $_SESSION['current_session_schedule'] = $_POST['schedule'];
+
+        while ($row = $result->fetch_assoc()) {
+
+            $id = $row['sch_id'];
+            $name = $row['title'];
+            if ($_SESSION['current_session_schedule']) {
+                if ($id == $_SESSION['current_session_schedule']) $form_data_shift_list .= '<option selected value="' . $id . '">' . $name . '</option>';
+                else $form_data_shift_list .= '<option value="' . $id . '">' . $name . '</option>';
+            } else $form_data_shift_list .= '<option value="' . $id . '">' . $name . '</option>';
+
+        }
+        $form_data_shift_list .= '</select></form>';
+
+        echo $form_data_shift_list;
+
+        if (isset($_GET['status_message'])) echo '<h3 style="color: #0F0;">' . $_GET['status_message'] . '</h3>';
+
+
+        if (isset($_SESSION['current_session_schedule'])) {
+
+            $schedule = $_SESSION['current_session_schedule'];
+
+            $locationArray = getLocations($db);
+            echo "<form><div class=\"row gtr-uniform\"><div class=\"col-12\"><ul class=\"nav nav-tabs\">";
+            $counter = 0;
+            foreach ($locationArray as $location) {
+                if (isset($_GET['location'])) {
+                    if ($_GET['location'] == $location) {
+                        echo "<li class=\"nav-item\">
     <a class=\"nav-link active pool\" href=\"?location=$location\">$location</a></li>";
+                    } else {
+                        echo "<li class=\"nav-item\">
+    <a class=\"nav-link pool\" href=\"?location=$location\">$location</a></li>";
+                    }
                 } else {
-                    echo "<li class=\"nav-item\">
-    <a class=\"nav-link pool\" href=\"?location=$location\">$location</a></li>";
-                }
-            } else {
-                if ($counter == 0){
-                    $_GET['location'] = $location;
-                    echo "<li class=\"nav-item\">
+                    if ($counter == 0) {
+                        $_GET['location'] = $location;
+                        echo "<li class=\"nav-item\">
     <a class=\"nav-link pool active\" href=\"?location=$location\">$location</a></li>";
-                }else {
-                    echo "<li class=\"nav-item\">
+                    } else {
+                        echo "<li class=\"nav-item\">
     <a class=\"nav-link pool\" href=\"?location=$location\">$location</a></li>";
+                    }
                 }
             }
-        }
 
-        echo "</ul></div></div></form>";
+            echo "</ul></div></div></form>";
 
-        if (isset($_GET['week'])) {
-            //Table for Schedule
-            echo "<br/><br/><div class='row'><div class='col-2'>
+            if (isset($_GET['week'])) {
+                //Table for Schedule
+                echo "<br/><br/><div class='row'><div class='col-2'>
 <nav class=\"nav flex-column nav-pills\">
   <a id='WD0' class=\"nav-link table active\">Dimanche</a>
             <a id='WD1' class=\"nav-link table\">Lundi</a>
@@ -63,68 +89,69 @@ if (isset($_SESSION['user_id'])) {
 </nav>
 </div>";
 
-            echo "<div class='col-7'><div id='WD0T' class='col-12 table' style='display: none'>";
-            $currentLocation = $locationArray[5];
-            if (isset($_GET['location'])) {
-                $currentLocation = $_GET['location'];
-            }
+                echo "<div class='col-7'><div id='WD0T' class='col-12 table' style='display: none'>";
+                $currentLocation = $locationArray[5];
+                if (isset($_GET['location'])) {
+                    $currentLocation = $_GET['location'];
+                }
 
-            $schedule = 1;
+                $schedule = 1;
 
-            echo printWeekDayTable('Dimanche', $schedule, $db, $currentLocation);
+                echo printWeekDayTable('Dimanche', $schedule, $db, $currentLocation);
 
-            echo "</div>";
-            echo "<div id='WD1T' class='col-12 table'  style='display: none'>";
+                echo "</div>";
+                echo "<div id='WD1T' class='col-12 table'  style='display: none'>";
 
-            echo printWeekDayTable('Lundi', $schedule, $db, $currentLocation);
+                echo printWeekDayTable('Lundi', $schedule, $db, $currentLocation);
 
-            echo "</div>";
-
-
-            echo "<div id='WD2T' class='col-12 table'  style='display: none'>";
-
-            echo printWeekDayTable('Mardi', $schedule, $db, $currentLocation);
-
-            echo "</div>";
+                echo "</div>";
 
 
-            echo "<div id='WD3T' class='col-12 table'  style='display: none'>";
+                echo "<div id='WD2T' class='col-12 table'  style='display: none'>";
 
-            echo printWeekDayTable('Mercredi', $schedule, $db, $currentLocation);
+                echo printWeekDayTable('Mardi', $schedule, $db, $currentLocation);
 
-            echo "</div>";
-
-
-            echo "<div id='WD4T' class='col-12 table'  style='display: none;'>";
-
-            echo printWeekDayTable('Jeudi', $schedule, $db, $currentLocation);
-
-            echo "</div>";
+                echo "</div>";
 
 
-            echo "<div id='WD5T' class='col-12 table' style='display: none'>";
+                echo "<div id='WD3T' class='col-12 table'  style='display: none'>";
 
-            echo printWeekDayTable('Vendredi', $schedule, $db, $currentLocation);
+                echo printWeekDayTable('Mercredi', $schedule, $db, $currentLocation);
 
-            echo "</div>";
+                echo "</div>";
 
 
-            echo "<div id='WD6T' class='col-12 table' style='display: none'>";
+                echo "<div id='WD4T' class='col-12 table'  style='display: none;'>";
 
-            echo printWeekDayTable('Samedi', $schedule, $db, $currentLocation);
+                echo printWeekDayTable('Jeudi', $schedule, $db, $currentLocation);
 
-            echo "</div></div>";
+                echo "</div>";
 
-            echo "<div class='col-3'><div class=\"box\" style='background: rgba(99, 116, 133, 0.075); border-color: rgba(99, 116, 133, 0.25);'>";
 
-            //Put lifeguRD MANAGEMENT HERE
-            echo "<h5>Sauveteur</h5>";
+                echo "<div id='WD5T' class='col-12 table' style='display: none'>";
 
-            echo "</div></div>";
+                echo printWeekDayTable('Vendredi', $schedule, $db, $currentLocation);
 
-            echo "</div>";
-            echo "</div>
+                echo "</div>";
+
+
+                echo "<div id='WD6T' class='col-12 table' style='display: none'>";
+
+                echo printWeekDayTable('Samedi', $schedule, $db, $currentLocation);
+
+                echo "</div></div>";
+
+                echo "<div class='col-3'><div class=\"box\" style='background: rgba(99, 116, 133, 0.075); border-color: rgba(99, 116, 133, 0.25);'>";
+
+                //Put lifeguRD MANAGEMENT HERE
+                echo "<h5>Sauveteur</h5>";
+
+                echo "</div></div>";
+
+                echo "</div>";
+                echo "</div>
 					</section>";
+            }
         }
     } else {
         $content = '<h3>Vous n\'avez pas les permissions requises pour acc&egrave;der a cette page.</h3>';
