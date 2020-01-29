4f198639862b2ae4d166570d83dd616a26de3320
Horia Cristian Sandu <horiasandu1@gmail.com>
2020-01-29 13:43:06 -0500

    Fixed missing dir

---
+++ schedule_operations/createSchedule.php
@@ -0,0 +1,42 @@
+<?php
+define("PREAMBLE", "../");
+include(PREAMBLE . "db_operations/connection.php");
+include('../db_operations/db_functions.php');
+include(PREAMBLE . "assets/php/code_blocks.php");
+
+
+?>
+
+
+<?php
+
+
+$content = "<!DOCTYPE HTML><html>";
+
+
+if ($_POST["title"] == null || $_POST["sch_start_date"] == null || $_POST["sch_end_date"] == null || $_POST["avail_deadline"] == null) {
+    $content .= "Erreur: Un titre doit exister (Exemple: Hiver 2020) et les trois dates doivent exister ";
+
+    $content .= '<br><a href="schedule_creation.php">Retour</a>';
+} else {
+    insertNewSchedule($db, $_POST["sch_start_date"], $_POST["sch_end_date"], $_POST["avail_deadline"], $_POST["title"]);
+    $content .= "Horaire g&eacute;n&eacute;r&eacute; avec succ&egrave;s";
+
+    $content .= '<br><a href="schedule_creation.php">Retour</a>';
+}
+
+block_print_document_header("Template", PREAMBLE);
+$content .= "<body class=\"is-preload\">";
+$content .= "<div id=\"page-wrapper\">";
+block_print_header("", PREAMBLE);
+//TODO: put logic to verify cookie and modify this
+block_print_nav("<li><a href=\"" . PREAMBLE . "login.php\">Connexion</a></li>");
+
+//put code in content
+block_print_main($content);
+
+block_print_copyright();
+$content .= "    </div>";
+block_print_scripts(PREAMBLE);
+$content .= "	</body>";
+$content .= "</html>";
---
+++ schedule_operations/delete.php
@@ -0,0 +1,56 @@
+<?php
+define("PREAMBLE", "../");
+include(PREAMBLE . "db_operations/connection.php");
+include(PREAMBLE . "db_operations/db_functions.php");
+include(PREAMBLE . "assets/php/code_blocks.php");
+$sessionsArray = getSessions($db);
+if (isset($_GET["sch_id"])) {
+    $id = $_GET["sch_id"];
+}
+if (isset($_POST['confirm'])) {
+    $id = $_POST['confirmId'];
+    $content = "<h2>Deleted</h2>";
+    deleteExistingShifts($db, $id);
+    deleteSession($db, $id);
+    header("location:schedule_modification.php");
+
+} else {
+    $content = "<h2>ATTENTION: LA SUPPRESSION D'UNE SESSION EST IRR&Eacute;VERSIBLE. VOULEZ VOUS CONTINUER?</h2>
+    <form action='delete.php' method='post'>
+    <input type='hidden' name='confirm' value='confirmed'>
+    <input type='hidden' name='confirmId' value=$id>
+    <input type='submit' value='Confirmer'><br><br><a style='' href='schedule_modification.php'>Retour</a>";
+}
+
+
+//TODO add option to create new semester and add files to it
+
+
+$content .= "<!DOCTYPE HTML><html>";
+block_print_document_header("Template", PREAMBLE);
+$content .= "<body class=\"is-preload\">";
+$content .= "<div id=\"page-wrapper\">";
+block_print_header("", "../");
+
+session_start();
+if (isset($_SESSION['user_id'])) {
+    block_print_nav("");
+    //TODO VALIDATE USER ACCESS
+    block_print_main($content);
+} else {
+    block_print_nav("<li><a href='" . PREAMBLE . "login.php'>Connexion</a></li>");
+
+    block_print_main("<h2>D&eacute;sol&eacute;, la session est expir&eacute;e ou inexistante</h2>" . $connection_form);
+    session_destroy();
+}
+//put code in content
+
+
+block_print_copyright();
+$content .= "    </div>";
+block_print_scripts("../");
+$content .= "	</body>";
+$content .= "</html>";
+
+
+?>
