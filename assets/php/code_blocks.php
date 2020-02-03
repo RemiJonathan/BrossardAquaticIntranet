19285258c7c62b0d348145d0ccf49948e3e11d7b
RemiJonathan <contact@remijonathan.com>
2020-02-03 01:44:57 -0500

    God-like progress to scheduling module

--- assets/php/code_blocks.php
+++ assets/php/code_blocks.php
@@ -14,7 +14,7 @@ function block_print_document_header($title, $preamble)
 		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\" />
 		<link href=\"//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css\" rel=\"stylesheet\">
 		<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">
-		<link rel=\"stylesheet\" href=\"".$preamble."assets/css/main.css\" />
+		<link rel=\"stylesheet\" href=\"" . $preamble . "assets/css/main.css\" />
 	</head>
     ";
 }
@@ -23,11 +23,12 @@ function block_print_document_header($title, $preamble)
  * @param $class String to use optional class if wanted
  * @param $preamble String to use preamble
  */
-function block_print_header($class, $preamble){
+function block_print_header($class, $preamble)
+{
     echo "
     <!-- Header -->
 	<header $class id=\"header\">
-		<span class=\"logo\"><a href=\"".$preamble."index.php\">Brossard <span>Secteur Aquatique</span></a></span>
+		<span class=\"logo\"><a href=\"" . $preamble . "index.php\">Brossard <span>Secteur Aquatique</span></a></span>
 		<a href=\"#menu\"><span>Menu</span></a>
 	</header>
     ";
@@ -36,30 +37,31 @@ function block_print_header($class, $preamble){
 /**
  * @param $tabs String of <li> based on the user's connection cookie
  */
-function block_print_nav($tabs){
+function block_print_nav($tabs)
+{
     $user_id = -1;
-    $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_DATABASE);
+    $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
-    if(isset($_SESSION['user_id'])) {
+    if (isset($_SESSION['user_id'])) {
         $user_id = $_SESSION['user_id'];
-        $get_user_data_sql = "SELECT * FROM user WHERE user_id = '".$user_id."'";
+        $get_user_data_sql = "SELECT * FROM user WHERE user_id = '" . $user_id . "'";
         $get_user_data_res = $db->query($get_user_data_sql);
         $user_id = $get_user_data_res->fetch_array()['user_id'];
     }
 
-    if (check_user_permissions($user_id, 1)){
+    if (check_user_permissions($user_id, 1)) {
         //Todo: add all user tabs
-        $tabs.="<li><a href='".PREAMBLE."schedule.php'>Mon horaire</a></li>";
+        $tabs .= "<li><a href='" . PREAMBLE . "schedule.php'>Mon horaire</a></li>";
 
-    }else if (check_user_permissions($user_id, 2)){
+    } else if (check_user_permissions($user_id, 2)) {
         //Todo: add all admin tabs
-        $tabs ="<li><a href='".PREAMBLE."qualification/'>Gestion de qualifications</a></li>".$tabs;
-        $tabs = "<li><a href='".PREAMBLE."file_operations/document_update.php'>base de donn&eacute;es</a></li>".$tabs;
+        $tabs = "<li><a href='" . PREAMBLE . "qualification/'>Gestion de qualifications</a></li>" . $tabs;
+        $tabs = "<li><a href='" . PREAMBLE . "file_operations/document_update.php'>base de donn&eacute;es</a></li>" . $tabs;
         $tabs = "<li><a href='" . PREAMBLE . "shift/'>Gestion de quart</a></li>" . $tabs;
         $tabs = "<li><a href='" . PREAMBLE . "schedule_operations/schedule_modification.php'>Sessions</a></li>" . $tabs;
         $tabs = "<li><a href='" . PREAMBLE . "schedule/'>Gestion d'horaire</a></li>" . $tabs;
 
-    }else{
+    } else {
         //todo: add all disconnected tabs
 
     }
@@ -77,8 +79,9 @@ function block_print_nav($tabs){
     ";
 }
 
-function block_print_banner(){
-    echo"
+function block_print_banner()
+{
+    echo "
     <!-- Banner -->
 					<section id=\"banner\">
 						<div class=\"inner\">
@@ -97,7 +100,8 @@ function block_print_banner(){
 /**
  * @param $content String use other blocks to generate the page content
  */
-function block_print_main($content){
+function block_print_main($content)
+{
     echo "
     <!-- Main -->
 					<section class=\"wrapper style2\" id=\"main\">
@@ -108,7 +112,7 @@ function block_print_main($content){
     ";
 }
 
-$connection_form = "<h3>Connexion</h3><form action=\"".PREAMBLE."validate_connection.php\" method=\"post\">
+$connection_form = "<h3>Connexion</h3><form action=\"" . PREAMBLE . "validate_connection.php\" method=\"post\">
 												<div class=\"row gtr-uniform\">
 													<div class=\"col-12\">
 														<input id=\"username\" name=\"username\" placeholder=\"Name\" type=\"text\" value=\"\" />
@@ -118,7 +122,7 @@ $connection_form = "<h3>Connexion</h3><form action=\"".PREAMBLE."validate_connec
 														<input id=\"password\" name=\"password\" placeholder=\"Password\" type=\"password\" value=\"\" />
 													</div><input type='hidden' name='currentUrl' id='currentUrl'>
 													
-													<a href=\"".PREAMBLE."forgot_password.php\">Mot de passe oublié?</a>
+													<a href=\"" . PREAMBLE . "forgot_password.php\">Mot de passe oublié?</a>
 													<!-- Break -->
 													<div class=\"col-12\">
 														<ul class=\"actions\">
@@ -134,7 +138,8 @@ $connection_form = "<h3>Connexion</h3><form action=\"".PREAMBLE."validate_connec
  * @param $notifications String notifications related to the user
  * @return String
  */
-function block_print_welcome_header($user, $notifications){
+function block_print_welcome_header($user, $notifications)
+{
     return "
                             <header class=\"major special\">
 								<h1>Bienvenue $user!</h1>
@@ -171,7 +176,8 @@ function block_print_footer()
     ";
 }
 
-function block_print_copyright(){
+function block_print_copyright()
+{
     echo "
     <!-- Copyright -->
 	<section class=\"wrapper style2 copyright\">
@@ -186,16 +192,17 @@ function block_print_copyright(){
 /**
  * @param $preamble String of the link toward the root directory
  */
-function block_print_scripts($preamble){
-    echo"
+function block_print_scripts($preamble)
+{
+    echo "
     <!-- Scripts -->
-			<script src=\"".$preamble."assets/js/jquery.min.js\"></script>
-			<script src=\"".$preamble."assets/js/jquery.scrollex.min.js\"></script>
-			<script src=\"".$preamble."assets/js/jquery.scrolly.min.js\"></script>
-			<script src=\"".$preamble."assets/js/browser.min.js\"></script>
-			<script src=\"".$preamble."assets/js/breakpoints.min.js\"></script>
-			<script src=\"".$preamble."assets/js/util.js\"></script>
-			<script src=\"".$preamble."assets/js/main.js\"></script>
+			<script src=\"" . $preamble . "assets/js/jquery.min.js\"></script>
+			<script src=\"" . $preamble . "assets/js/jquery.scrollex.min.js\"></script>
+			<script src=\"" . $preamble . "assets/js/jquery.scrolly.min.js\"></script>
+			<script src=\"" . $preamble . "assets/js/browser.min.js\"></script>
+			<script src=\"" . $preamble . "assets/js/breakpoints.min.js\"></script>
+			<script src=\"" . $preamble . "assets/js/util.js\"></script>
+			<script src=\"" . $preamble . "assets/js/main.js\"></script>
 			<script>$(document).ready(function (){
 			$('#currentUrl').val(window.location.href);
 			});</script>
@@ -207,13 +214,14 @@ function block_print_scripts($preamble){
  * @param $permission_level int of required permission
  * @return boolean see if the user has appropriate permissions
  */
-function check_user_permissions($user_id, $permission_level){
-    $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD, DB_DATABASE);
+function check_user_permissions($user_id, $permission_level)
+{
+    $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
     $check_permission_sql = $db->query("SELECT role_id FROM user WHERE user.user_id = '$user_id';", MYSQLI_ASSOC);
 
     $user = $check_permission_sql->fetch_array();
 
-    return  $user['role_id'] == $permission_level;
+    return $user['role_id'] == $permission_level;
 }
 
 /**
@@ -224,7 +232,8 @@ function check_user_permissions($user_id, $permission_level){
  * @param $location
  * @return string HTML table
  */
-function printWeekDayTable($selectedWeekDay, $schedule, $db,$location){
+function printWeekDayTable($selectedWeekDay, $schedule, $db, $location)
+{
     $content = "";
     $content .= "<table id='$selectedWeekDay' style='empty-cells: show; table-layout: fixed; width: 100%;' class=\"alt\">
     <tbody>
@@ -234,7 +243,7 @@ function printWeekDayTable($selectedWeekDay, $schedule, $db,$location){
     $content .= '<tr><th></th></tr>';
 
 
-    switch ($location){
+    switch ($location) {
         case 'LT':
             $this_weeks_shifts_sql = "SELECT * FROM shift WHERE location REGEXP 'LT|LT-SALLE|PIS-LT' AND schedule_id = '$schedule' AND day = '$selectedWeekDay'";
             break;
@@ -249,7 +258,7 @@ function printWeekDayTable($selectedWeekDay, $schedule, $db,$location){
 
     $this_weeks_earliest_shift_sql = $this_weeks_shifts_sql . " ORDER BY start_time";
     $this_weeks_latest_ending_shift_sql = $this_weeks_shifts_sql . " ORDER BY end_time DESC";
-    $this_weeks_shifts_res = $db->query($this_weeks_shifts_sql." ORDER BY start_time ASC, end_time DESC");
+    $this_weeks_shifts_res = $db->query($this_weeks_shifts_sql . " ORDER BY start_time ASC, end_time DESC");
 
     echo "<!--$this_weeks_shifts_sql ORDER BY start_time ASC, end_time DESC-->";
 
@@ -267,31 +276,36 @@ function printWeekDayTable($selectedWeekDay, $schedule, $db,$location){
         $shift_array[$index] = $shift;
 
         $duration = round(abs($end_time - $start_time) / 60, 2);
-        $shift_array[$index]['html'] = '<td class="shift" id="' . $shift['shift_id'] . '" rowspan="' . (($duration / 5)) . '">' . $shift['description'];
+        $shift_array[$index]['html'] = '<td class="shift';
+        if (!is_null($shift['assigned_user'])) $shift_array[$index]['html'] .= ' user' . $shift['assigned_user'] . ' selected';
+        $shift_array[$index]['html'] .= '" rowspan="' . (($duration / 5)) . '" data-start="'.$shift['start_time'].'" data-end="'.$shift['end_time'].'">' . $shift['description'];
 
-        if (!is_null($shift['assigned_user'])) echo '<br />' . $shift['assigned_user'];
+        //if (!is_null($shift['assigned_user'])) echo '<br />' . $shift['assigned_user'];
 
-        echo /*date('H:i',$start_time).' - '.date('H:i',$end_time).*/'</td>';
+        $shift_array[$index]['html'] .= /*date('H:i',$start_time).' - '.date('H:i',$end_time).*/
+            '</td>';
 
         $shift_array[$index]['duration'] = $duration;
 
         $index++;
     }
 
-$counter = 0;
+    $counter = 0;
     for ($i = strtotime($table_begin); $i < strtotime($table_end); $i += (5 * 60)) {
         $content .= "<tr>";
         for ($j = 0; $j < 2; $j++) {
             switch ($j) {
                 case 0:
                     if ($i % 1800 == 0) {
-                        $content .= "<th  style='padding: 4em;' rowspan='6'>" . date('G:i', $i) . "</th>"; $counter ++;
+                        $content .= "<th  style='padding: 4em;' rowspan='6'>" . date('G:i', $i) . "</th>";
+                        $counter++;
                     } else {
                         if (strtotime($table_begin) % 1800 != 0 && ($i - strtotime($table_begin) < 1800) && $counter == 0) {
-                            $content .= "<th rowspan='".(6-(($i % 1800)/300))."'></th>";$counter++;
-                        }else{
-                        $content .= "<th style='display: none'></th>";
-}
+                            $content .= "<th rowspan='" . (6 - (($i % 1800) / 300)) . "'></th>";
+                            $counter++;
+                        } else {
+                            $content .= "<th style='display: none'></th>";
+                        }
                     }
                     break;
                 default:
@@ -308,7 +322,8 @@ $counter = 0;
                             }
                         }
                     }
-                    break;}
+                    break;
+            }
 
         }
 
@@ -325,19 +340,20 @@ $counter = 0;
  * @param $seniority_type
  * @param $db
  */
-function print_seniority_dropdown($seniority_type, $db){
+function print_seniority_dropdown($seniority_type, $db)
+{
     $get_seniority_sql = "SELECT * FROM seniority LEFT OUTER JOIN user u on seniority.user_id = u.user_id WHERE seniority_type = '$seniority_type' ORDER BY hours DESC, continued_service;";
     $get_seniorty_res = $db->query($get_seniority_sql);
 
-    echo"<ol class=\"list-group $seniority_type\" style='max-height: 250px; overflow-y:scroll; display: none'>";
+    echo "<ol class=\"list-group $seniority_type\" style='max-height: 250px; overflow-y:scroll; display: none'>";
 
-    while ($user = $get_seniorty_res->fetch_array(MYSQLI_ASSOC)){
-        $id = 'user'.$user['user_id'];
-        echo"<li id='$id' class='list-group-item list-group-item-action' style='font-size: small; '>";
+    while ($user = $get_seniorty_res->fetch_array(MYSQLI_ASSOC)) {
+        $id = 'user' . $user['user_id'];
+        echo "<li id='$id' class='list-group-item list-group-item-action' style='font-size: small; '>";
         //echo $user['user_id']."<br />";
-        echo $user['user_fname']." ".$user['user_lname'];
-        echo "<br /><strong>" .$user['hours']."</strong>";
-        echo"</li>";
+        echo $user['user_fname'] . " " . $user['user_lname'];
+        echo "<br /><strong>" . $user['hours'] . "</strong>";
+        echo "</li>";
     }
 
     echo "</ol>";
