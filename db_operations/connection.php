569074ef4eb5306000ca4bcff66579bbf61ff44e
Horia Cristian Sandu <horiasandu1@gmail.com>
2020-02-08 12:20:20 -0500

    000webhosting test

--- db_operations/connection.php
+++ db_operations/connection.php
@@ -1,11 +1,9 @@
 <?php
 //UChNLJEjh23vrjWr
-/*
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'regular_user');
 define('DB_PASSWORD', 'UChNLJEjh23vrjWr');
 define('DB_DATABASE', 'brossard_intranet');
-*/
 
 
 $db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
